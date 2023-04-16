0e09819f255f691a9ac0d3648a9c4e9bea8f4892
Rakesh <rakeshtiwari887429@gmail.com>
2023-04-16 23:26:24 +0530

    Solution Added

--- src/SortingCities.java
+++ src/SortingCities.java
@@ -1,5 +1,63 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+public class SortingCities {
+    public String[] citiesWithDistance(String[]cities, int[] distance) {
+        int len= distance.length;
+        int temp;
+        String str;
+        for(int i=0;i<len-1;i++){
+            for(int j=0;j<len-1-i;j++){
+                if(distance[j]>distance[j+1]){
+                    //Swap Distance
+                    temp=distance[j];
+                    distance[j]=distance[j+1];
+                    distance[j+1]=temp;
+                   //swap cities
+                    str=cities[j];
+                    cities[j]=cities[j+1];
+                    cities[j+1]=str;
+                }
+            }
+        }
+        String []upperCaseCity = new String[cities.length];
+        for(int i = 0; i<cities.length;i++){
+        upperCaseCity[i] = cities[i].toUpperCase();
+        }
+        return upperCaseCity;
     }
-}
+    public String findCity(String[]cities, int []distance){
+           int index = 0;
+            int d = 52;
+            for(int i = 0; i<distance.length; i++){
+                if(distance[i]==d){
+                    d = distance[i];
+                    index = i;
+                }
+            }
+            return cities[index];
+    }
+    public String findHighestCity(String []cities, int []distance){
+        int index=0;
+        int d = 270;
+        for(int i = 0; i<distance.length;i++){
+            if(distance[i]>d){
+                d=distance[i];
+                index = i;
+            }
+        }
+        return cities[index];
+    }
+
+    public static void main (String[]args){
+            String[] cities = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+            int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+            SortingCities sortCity = new SortingCities();
+               String[]sortedCities = sortCity.citiesWithDistance(cities,distance);
+               System.out.println(Arrays.toString(sortedCities));
+               String cityName = sortCity.findCity(sortedCities,distance);
+               System.out.println("City Which Is Having 52KM distance : "+cityName);
+               String cityName1 = sortCity.findHighestCity(sortedCities,distance);
+               System.out.println("City Which Has Distance More Than 270KM : "+cityName1);
+    }
+    }
+
--- src/SortingCitiesTest.java
+++ src/SortingCitiesTest.java
@@ -1,4 +1,37 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SortingCitiesTest {
-  
+    SortingCities obj;
+    @Before
+    public void setUp(){
+      obj = new SortingCities();
+    }
+    @After
+    public void tearDown(){
+        obj = null;
+    }
+    @Test
+    public void citiesWithDistanceTest(){
+        SortingCities obj = new SortingCities();
+        String []str = {"LUCERNE", "ENGELBERG", "BASEL", "JUNGFRAUJOCH", "MURREN", "INTERLAKEN", "GRINDELWALD", "BERN", "ZERMATT", "GENEVA"};
+        assertArrayEquals(str,obj.citiesWithDistance(new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"},new int[]{138, 52, 118, 136, 85, 276, 103, 87, 214, 101}));
+    }
+    @Test
+    public void fineCityTest(){
+        SortingCities obj = new SortingCities();
+        assertEquals("Lucerne",obj.findCity(new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"},new int[]{138, 52, 118, 136, 85, 276, 103, 87, 214, 101}));
+    }
+    @Test
+    public void findHighestCity(){
+        SortingCities obj = new SortingCities();
+        assertEquals("Geneva",obj.findHighestCity(new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"},new int[]{138, 52, 118, 136, 85, 276, 103, 87, 214, 101}));
+
+    }
+
 }
